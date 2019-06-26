727529bdfb1c0fb2e3f11a8d2be0b05c0dbcf338
Rohan Taneja <rohan.bvcoe@gmail.com>
2019-06-26 21:50:31 +0200

    hookup firebase_auth with flutter_twitter_login

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_twitter_login/flutter_twitter_login.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -7,6 +8,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  FirebaseAuth _auth = FirebaseAuth.instance;
   String message = "defaultmessage";
 
   @override
@@ -44,6 +46,7 @@ class _HomePageState extends State<HomePage> {
     switch (result.status) {
       case TwitterLoginStatus.loggedIn:
         newMessage = 'Logged in! username: ${result.session.username}';
+        _signInWithTwitter(result.session.token, result.session.secret);
         break;
       case TwitterLoginStatus.cancelledByUser:
         newMessage = 'Login cancelled by user.';
@@ -58,8 +61,30 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  void _signInWithTwitter(String token, String secret) async {
+    final AuthCredential credential = TwitterAuthProvider.getCredential(
+        authToken: token, authTokenSecret: secret);
+    final FirebaseUser user = await _auth.signInWithCredential(credential);
+    assert(user.email != null);
+    assert(user.displayName != null);
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+    setState(() {
+      if (user != null) {
+        message = 'Successfully signed in with Twitter. ' + user.uid;
+      } else {
+        message = 'Failed to sign in with Twitter. ';
+      }
+    });
+  }
+
   void _logout() async {
     await twitterLogin.logOut();
+
+    await _auth.signOut();
 
     setState(() {
       message = 'Logged out.';
