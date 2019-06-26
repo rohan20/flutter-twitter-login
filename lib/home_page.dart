285dfdc393907fa64d6aae6beace0bccc7c3493a
Rohan Taneja <rohan.bvcoe@gmail.com>
2019-06-26 20:44:11 +0200

    setup ui

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,8 +6,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String message = "default_message";
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text("Login"),
+              onPressed: () {},
+            ),
+            RaisedButton(
+              child: Text("Logout"),
+              onPressed: () {},
+            ),
+            SizedBox(height: 32.0),
+            Text(message),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:twitter_login_sample/home_page.dart';
 
-void main() => runApp(HomePage());
+void main() => runApp(MaterialApp(home: HomePage()));
